8ef0abd54beec6ee0121cedc17dc02a2e457b9ec
welhoilija <tkankas@gmail.com>
2023-08-21 00:02:22 +0300

    Update reservations endpoint to include reviews as well for the reservations

--- backend/endpoints/reservations.py
+++ backend/endpoints/reservations.py
@@ -1,15 +1,15 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from database import get_db
-from models.models import Reservation
-from schemas import ReservationCreate, ReservationListResponse, ReservationList
+from models.models import Reservation, Review as DBReview
+from schemas import ReservationSchema, ReservationListResponse, ReservationList, Review
 from dataclasses import asdict
 
 router = APIRouter()
 
 
 @router.post("/create_reservation/")
-async def create_reservation(request: ReservationCreate, db: Session = Depends(get_db)):
+async def create_reservation(request: ReservationSchema, db: Session = Depends(get_db)):
     db_reservation = Reservation(**asdict(request))
     db.add(db_reservation)
     db.commit()
@@ -21,15 +21,23 @@ async def create_reservation(request: ReservationCreate, db: Session = Depends(g
 async def list_reservations(db: Session = Depends(get_db)):
     reservations = db.query(Reservation).all()
 
-    reservation_list = [
-        ReservationList(
-            id=res.id,
-            restaurant_name=res.restaurant_name,
-            datetime=res.datetime.strftime(
-                '%Y-%m-%d %H:%M:%S'),
-            description=res.description,
-        )
-        for res in reservations
-    ]
+    reservation_list = []
+    for res in reservations:
+        reviews = db.query(DBReview).filter_by(reservation_id=res.id).all()
+        review_list = [
+            Review(id=review.id, stars=review.stars, comment=review.comment)
+            for review in reviews
+        ]
 
-    return ReservationListResponse(reservations=reservation_list)
+        reservation_list.append(
+            ReservationList(
+                id=res.id,
+                restaurant_name=res.restaurant_name,
+                datetime=res.datetime.strftime('%Y-%m-%d %H:%M:%S'),
+                description=res.description,
+                reviews=review_list
+            )
+        )
+
+    response_data = ReservationListResponse(reservations=reservation_list)
+    return response_data
