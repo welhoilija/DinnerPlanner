e6e9a2eac60049193bf09fc239bcfd8b49f5fe01
welhoilija <tkankas@gmail.com>
2023-09-03 18:45:13 +0300

    update database connection stuff

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,7 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
 import os
-import logging
+from sqlalchemy.orm import Session
 
+import sqlalchemy
 
 DB_USER = os.environ.get("POSTGRES_USER")
 DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
@@ -10,18 +9,37 @@ DB_HOST = os.environ.get("POSTGRES_HOST", "db")
 DB_PORT = os.environ.get("POSTGRES_PORT", "5432")
 DB_NAME = os.environ.get("POSTGRES_DB")
 
-logging.warning(f"DB_USER: {DB_USER}")
-logging.warning(f"DB_PASSWORD: {DB_PASSWORD}")
-logging.warning(f"DB_HOST: {DB_HOST}")
-logging.warning(f"DB_PORT: {DB_PORT}")
-logging.warning(f"DB_NAME: {DB_NAME}")
-DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
-engine = create_engine(DATABASE_URL)
+def connect_tcp_socket() -> sqlalchemy.engine.base.Engine:
+    """Initializes a TCP connection pool for a Cloud SQL instance of Postgres."""
+    # Note: Saving credentials in environment variables is convenient, but not
+    # secure - consider a more secure solution such as
+    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
+    # keep secrets safe.
+    db_host = DB_HOST
+    db_user = DB_USER
+    db_pass = DB_PASSWORD
+    db_name = DB_NAME
+    db_port = DB_PORT
+
+    pool = sqlalchemy.create_engine(
+        # Equivalent URL:
+        # postgresql+pg8000://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
+        sqlalchemy.engine.url.URL.create(
+            drivername="postgresql+pg8000",
+            username=db_user,
+            password=db_pass,
+            host=db_host,
+            port=db_port,
+            database=db_name,
+        ),
+        # ...
+    )
+    return pool
 
 
 def get_db():
-    db = Session(bind=engine)
+    db = Session(bind=connect_tcp_socket())
     try:
         yield db
     finally:
